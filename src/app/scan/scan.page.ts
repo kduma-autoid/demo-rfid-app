c50f29b7c2bd6b97b4c40cebccb0be55c93f9122
Krystian Duma <git@krystian.duma.sh>
2023-08-16 20:20:59 +0200

    WIP

--- src/app/scan/scan.page.ts
+++ src/app/scan/scan.page.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import {Component, HostListener, NgZone, OnDestroy, OnInit, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {IonicModule} from '@ionic/angular';
+import {HandleableKey, KeyEvent, SunmiKeyboardHandler} from "@kduma-autoid/capacitor-sunmi-keyboard-handler";
+import {SunmiUHF} from "@kduma-autoid/capacitor-sunmi-uhf";
+// @ts-ignore
+import {valueOf as EpcTdsValueOf} from "epc-tds";
 
 @Component({
   selector: 'app-scan',
@@ -10,13 +14,107 @@ import { IonicModule } from '@ionic/angular';
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule]
 })
-export class ScanPage implements OnInit {
+export class ScanPage implements OnInit, OnDestroy {
 
   newTodo = 'test'
+  model = signal<string>('')
+  tags = signal<string[]>([]);
+  tagValues = signal<{epc: string, uri: string, type: string, barcode: string, serial: string}[]>([]);
+  reads = signal<number>(0);
+  rate = signal<number>(0);
+  scanning = signal<boolean>(false);
 
-  constructor() { }
+  constructor(private ngZone: NgZone) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.model.set((await SunmiUHF.getScanModel()).model);
+
+    await SunmiKeyboardHandler.setKeyHandler({key: HandleableKey.RFID}, async (event) => {
+      await this.ngZone.run(async () => {
+        if (event.type === KeyEvent.KeyDown) {
+          await this.startScan();
+        } else {
+          await this.stopScan();
+        }
+      });
+    });
+  }
+
+  async stopScan() {
+    await this.ngZone.run(async () => {
+      this.scanning.set(false);
+      await SunmiUHF.stopScanning();
+    });
+  }
+
+  async startScan() {
+    await this.ngZone.run(async () => {
+      this.tags.set([]);
+      this.tagValues.set([]);
+      this.reads.set(0);
+      this.rate.set(0);
+
+      this.scanning.set(true);
+
+      await SunmiUHF.startScanning();
+    });
+  }
+
+  @HostListener('window:sunmi_uhf_tag_read', ['$event'])
+  onTagRead(tag: {
+    epc: string;
+    pc: string;
+    frequency: string;
+    rrsi: string;
+    antenna: number;
+    last_updated: number;
+    read_count: number;
+  }) {
+    this.reads.update(value => ++value);
+
+    const epc = tag.epc;
+
+    if (this.tags().indexOf(epc) === -1) {
+      this.tags.mutate(values => values.push(epc));
+
+      try {
+        const val = EpcTdsValueOf(epc);
+        this.tagValues.mutate(values => values.push({
+          epc: epc,
+          uri: val.toTagURI(),
+          type: val.getType(),
+          barcode: val.toBarcode(),
+          serial: val.getSerial()
+        }));
+      } catch (e) {
+        this.tagValues.mutate(values => values.push({
+          epc: epc,
+          type: 'UNKNOWN',
+          uri: 'UNKNOWN',
+          barcode: 'UNKNOWN',
+          serial: 'UNKNOWN'
+        }));
+      }
+
+    }
+  }
+
+  @HostListener('window:sunmi_uhf_read_completed', ['$event'])
+  onInventoryScanCompleted(scan: {
+    rate: number;
+    tags_read: number;
+    details: {
+      start_time: number;
+      end_time: number;
+    };
+  }) {
+    this.rate.set(scan.rate);
+  }
+
+  async ngOnDestroy() {
+    await SunmiKeyboardHandler.removeKeyHandler({key: HandleableKey.RFID});
+    await SunmiUHF.clearTagReadCallback();
+    await SunmiUHF.clearInventoryScanCompletedCallback();
   }
 
 }
